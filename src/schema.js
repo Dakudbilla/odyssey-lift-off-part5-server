ee843a1912475454b2b447b30827f8d9b538ca8d
akuddev <akuddev@gmail.com>
2022-12-07 13:11:55 +0000

    [fix]: track update crash

--- src/schema.js
+++ src/schema.js
@@ -36,8 +36,7 @@ const typeDefs = gql`
     "The track's illustration to display in track card or track page detail"
     thumbnail: String
     "The track's approximate length to complete, in seconds"
-    length: Int @deprecated(reason: "Use durationInseconds")
-
+    length:  Int @deprecated(reason: "Use durationInSeconds")
     "The tracks  full duration in seconds"
     durationInSeconds: Int
     "The number of modules this track contains"
@@ -65,9 +64,9 @@ const typeDefs = gql`
     "The module's title"
     title: String!
     "The module's length in minutes"
-    length: Int @deprecated("Use length in seconds")
-    "The module's video duration in minutes"
-     durationInSeconds: Int
+    length:  Int @deprecated(reason: "Use durationInSeconds")
+    "The module's video duration in seconds"
+    durationInSeconds: Int
     "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
     content: String
     "The module's video url, for video-based modules"
